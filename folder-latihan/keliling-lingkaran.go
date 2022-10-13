411adaad0a7fcfadaba5b5cc58cb95a55e167d53
nameshafa <nameshafa@gmail.com>
2022-10-13 16:35:50 +0700

    Keliling lingkaran

---
+++ folder-latihan/keliling-lingkaran.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    
+    var spRadius, spKeliling float64
+
+    fmt.Print("Masukan radius lingkaran: ")
+    fmt.Scanln(&spRadius)
+
+    spSA = 4 * 3.14 * spRadius
+  
+    fmt.Println("Luas lingkaran  = ", spkeliling)
+}
